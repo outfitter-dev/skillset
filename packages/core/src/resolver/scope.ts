e75a76fa39e7d29644ce989aa17105bdc3fb73ae
Matt Galligan <matt.galligan@gmail.com>
2026-01-05 14:39:18 -0500

    chore: organize imports

--- packages/core/src/resolver/scope.ts
+++ packages/core/src/resolver/scope.ts
@@ -1,6 +1,6 @@
+import { inferToolFromPath } from "@skillset/shared";
 import type { ConfigSchema, Scope, Skill, SkillSet } from "@skillset/types";
 import { normalizeTokenSegment } from "../normalize";
-import { inferToolFromPath } from "@skillset/shared";
 
 /**
  * Namespace shortcuts for common scope aliases.
